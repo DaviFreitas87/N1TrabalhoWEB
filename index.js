616c46f57b72fb82235eae3fe1dc21d2b78c1fac
João Guilherme <guilherme@Mac-Os.local>
2019-08-22 13:54:24 -0300

    Pizzaria 2.0

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* Backend Pizzaria
+Davi Freitas,Guilherme Alves,João Guilherme */
 var express = require('express');
 var app = express();
 
@@ -9,10 +11,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Recebe e imprime os pedidos
 app.post('/elements.html', function(req, res){
     var conteudo = JSON.stringify(req.body);
     console.log("Pedido Recebido:\n" + conteudo);
 });
 
+// Inicia o servidor
 app.listen(8000);
 console.log("Rodando na porta 8000");
